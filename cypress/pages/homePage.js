8e96375dc2b266b87a2d48beae75ec02c96559ec
Anshita Bhasin <anshita.bhasin@gmail.com>
2023-08-26 13:05:44 +0400

    test failure

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -4,7 +4,7 @@ export class homePage {
     weblocators = {
 
         search_input: '.form-control.input-lg',
-        click_search: '.btn.btn-default.btn-lg',
+        click_search: '.btn.btn-default.btn-lg1',
         product: 'img[title="MacBook"]',
         addtocart: 'Add to Cart',
 
